e9ad541204d11a5b576288cac3e2e29ac6b89d35
Matheus Almeida <almeida-matheus@hotmail.com.br>
2019-05-06 19:19:06 -0300

    [react-gsap][feat] - adding tweenmax timeline to open and close drawer

--- react-gsap/src/components/drawer/index.jsx
+++ react-gsap/src/components/drawer/index.jsx
@@ -1,19 +1,53 @@
-import React from 'react'
+import React, { useMemo, useRef, useEffect } from 'react'
+import PropTypes from 'prop-types'
 import css from './style.module.scss'
 import { Button } from 'components'
+import { TimelineMax, Circ } from 'gsap'
 
-export default () => (
-    <div className={css.Drawer}>
-        <div className="container">
-            <div className="row justify-content-end">
-                <div className="col-auto">
-                    <Button
-                        onClick={() => {
-                            console.log('close contianer')
-                        }}
-                    />
+const Drawer = ({ isOpened, toggleIsOpened }) => {
+    const timelineMax = useRef()
+    const drawerRef = useRef()
+
+    useEffect(() => {
+        if (!timelineMax.current) {
+            timelineMax.current = new TimelineMax({ paused: true })
+            timelineMax.current.set(drawerRef.current, {
+                x: '100%',
+            })
+
+            timelineMax.current.to(drawerRef.current, 0.7, {
+                x: '0%',
+                ease: Circ.easeInOut,
+            })
+        }
+    }, [])
+
+    useMemo(() => {
+        if (timelineMax.current) {
+            if (isOpened) {
+                timelineMax.current.restart()
+            } else {
+                timelineMax.current.reverse()
+            }
+        }
+    }, [isOpened])
+
+    return (
+        <div id="testeeeeee" ref={drawerRef} className={css.Drawer}>
+            <div className="container">
+                <div className="row justify-content-end">
+                    <div className="col-auto">
+                        <Button onClick={toggleIsOpened} />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-)
+    )
+}
+
+Drawer.propTypes = {
+    isOpened: PropTypes.bool.isRequired,
+    toggleIsOpened: PropTypes.func.isRequired,
+}
+
+export default Drawer
--- react-gsap/src/entries/home/index.jsx
+++ react-gsap/src/entries/home/index.jsx
@@ -1,62 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, Button, Drawer } from 'components'
 import css from './style.module.scss'
 
-export default () => (
-    <section className={css.Home}>
-        <div className="container">
-            <Drawer />
-            <div
-                className={
-                    css['Home__actions-container'] + ' row justify-content-end'
-                }
-            >
-                <div className="col-auto">
-                    <Button
-                        onClick={() => {
-                            console.log('teste')
-                        }}
-                    />
+export default () => {
+    const [drawerIsOpened, setDrawerIsOpened] = useState(false)
+    const toggleIsOpened = () => setDrawerIsOpened(!drawerIsOpened)
+
+    return (
+        <section className={css.Home}>
+            <div className="container">
+                <Drawer
+                    isOpened={drawerIsOpened}
+                    toggleIsOpened={toggleIsOpened}
+                />
+                <div
+                    className={
+                        css['Home__actions-container'] +
+                        ' row justify-content-end'
+                    }
+                >
+                    <div className="col-auto">
+                        <Button onClick={toggleIsOpened} />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <Card type="I" />
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <Card type="II" />
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <Card type="III" />
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <Card type="IV" />
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <Card type="III" />
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <Card type="IV" />
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <Card type="II" />
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <Card type="IV" />
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <Card type="III" />
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <Card type="I" />
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <Card type="II" />
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <Card type="IV" />
+                    </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-12 col-sm-6 col-md-4">
-                    <Card type="I" />
-                </div>
-                <div className="col-12 col-sm-6 col-md-4">
-                    <Card type="II" />
-                </div>
-                <div className="col-12 col-sm-6 col-md-4">
-                    <Card type="III" />
-                </div>
-                <div className="col-12 col-sm-6 col-md-4">
-                    <Card type="IV" />
-                </div>
-                <div className="col-12 col-sm-6 col-md-4">
-                    <Card type="III" />
-                </div>
-                <div className="col-12 col-sm-6 col-md-4">
-                    <Card type="IV" />
-                </div>
-                <div className="col-12 col-sm-6 col-md-4">
-                    <Card type="II" />
-                </div>
-                <div className="col-12 col-sm-6 col-md-4">
-                    <Card type="IV" />
-                </div>
-                <div className="col-12 col-sm-6 col-md-4">
-                    <Card type="III" />
-                </div>
-                <div className="col-12 col-sm-6 col-md-4">
-                    <Card type="I" />
-                </div>
-                <div className="col-12 col-sm-6 col-md-4">
-                    <Card type="II" />
-                </div>
-                <div className="col-12 col-sm-6 col-md-4">
-                    <Card type="IV" />
-                </div>
-            </div>
-        </div>
-    </section>
-)
+        </section>
+    )
+}
